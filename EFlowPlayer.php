a5303a3ffbb20fc7618459cf29ca17781b1a4994
server@dimeng.home.gr <server@dimeng.home.gr>
2011-04-30 20:33:42 +0300

    version 0.2 alpha

--- EFlowPlayer.php
+++ EFlowPlayer.php
@@ -5,17 +5,40 @@
  * ===========
  * Yii extension for the [flowplayer](http://www.flowplayer.org)
  *
+ * ###Description 
+ * This is an alpha version of the extension. 
+ * It supports only the basic configuration.
+ *
  * ###Support
  * - Yii 1.1.x
  * - flowplayer 3.2.6
  *
  * This base code was generated with the [gii-extension-generator](http://www.yiiframework.com/extension/gii-extension-generator/)
- * @version 0.1
+ * @version 0.2 alpha
  * @author Dimitrios Mengidis
  */
 class EFlowPlayer extends CWidget
 {
-
+    /** The flv url.
+     * @var string
+     * @since 0.2
+     */
+    public $flv;
+    /** Tag element player will be.
+     * @var string
+     * @since 0.2
+     */
+    public $tag = 'div';
+    /** The flowplayer.swf url 
+     * @var string 
+     * @since 0.2
+     */
+    public $swfUrl;
+    /** The htmlOptions of the video
+     * @var array
+     * @since 0.2
+     */
+    public $htmlOptions;
     /** The js scripts to register.
      * @var array
      * @since 0.1
@@ -36,7 +59,6 @@ class EFlowPlayer extends CWidget
      * @since 0.1
      */
     private $assets;
-
     private function publishAssets() 
     {
         $assets = dirname(__FILE__).DIRECTORY_SEPARATOR."assets".DIRECTORY_SEPARATOR;
@@ -53,7 +75,7 @@ class EFlowPlayer extends CWidget
         }
         foreach($this->css as $file)
         {
-            $cs->registerCss($this->assets."/".$file);
+            $cs->registerCssFile($this->assets."/".$file);
         }
     }
 
@@ -61,8 +83,18 @@ class EFlowPlayer extends CWidget
     {
         $this->publishAssets();
         $this->registerScripts();
+
+        if(!isset($this->htmlOptions['id'])) $this->htmlOptions['id'] = $this->id;
+        if(!isset($this->swfUrl)) $this->swfUrl = $this->assets."/flowplayer-3.2.7.swf";
     }
     public function run()
     {
+        echo CHtml::openTag($this->tag, $this->htmlOptions);
+        echo CHtml::closeTag($this->tag);
+
+        Yii::app()->clientScript->registerScript($this->id, 
+            "flowplayer('".$this->htmlOptions['id']."','".$this->swfUrl."', '".$this->flv."')", 
+            CClientScript::POS_READY
+        );
     }
 }
